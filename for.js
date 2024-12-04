a11f838f2eee028824355d77e7abd6b2b3195b81
brant <brantandbrant>
2024-12-04 20:48:19 -0300

    selecionador ingredientes

---
+++ for.js
@@ -0,0 +1,47 @@
+
+// Procurando por uma fruta específica
+/*let frutas = ["Maçã", "Banana", "Laranja", "Manga"]
+let frutaProcurada = "Laranja"
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Verificando: ${frutas[i]}`)
+
+  if (frutas[i] === frutaProcurada) {
+    console.log(`Fruta encontrada: ${frutas[i]}`);
+    break; // Interrompe o loop ao encontrar a fruta
+  }
+}*/
+
+
+/*let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let soma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  soma += numeros[i]; // Adiciona o número atual à variável 'soma'
+}
+console.log("A soma dos números é:", soma);
+*/
+
+
+/*let valores = [2, 4, 6, 8];
+for (let i = 0; i < valores.length; i++) {
+  valores[i] *= 3.5; // Multiplica cada valor por 2
+}
+console.log("Array modificado:", valores);*/
+
+
+/*let numeros = [10, 20, 30]
+
+// Definindo uma função que será usada pelo forEach
+function exibirNumero(numero) {
+  console.log(`O número é: ${numero}`)
+}
+
+exibirNumero(numeros[0])
+exibirNumero(numeros[1])
+exibirNumero(numeros[2])
+
+// Usando o forEach para percorrer o array e chamar a função p/ cada elemento
+//numeros.forEach(exibirNumero)*/
+
+
